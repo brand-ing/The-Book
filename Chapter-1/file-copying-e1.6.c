21aa16ab5e5f8821c023d48954a318e816d8273c
chrome <brandontirving@gmail.com>
2024-05-18 21:07:57 -0400

    restructuring

---
+++ Chapter-1/file-copying-e1.6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/* copy input to output; 2nd version */
+int main()
+{
+    int c;
+    if (c != EOF) {
+        printf("The expression evaluated to 1.\n");
+    } else {
+        printf("The expression evaluated to 0.\n");
+    }
+    return 0;
+}
---
+++ Chapter-1/file-copying-e1.7.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+/* Write a program to print the value of EOF*/
+int main() {
+    printf("The value of EOF is: %d\n", EOF);
+    return 0;
+}
